04ae2e6d0adbc7ba21be580e4c54ae334ea56507
Snickers88 <zaharstarushkevuch@ukr.net>
2018-05-17 19:26:45 +0300

    remind

---
+++ app/src/main/java/com/example/snickers/auto/Reminder.java
@@ -0,0 +1,26 @@
+package com.example.snickers.auto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Reminder extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_reminder, container, false);
+
+        FloatingActionButton t = view.findViewById(R.id.floatingActionButton3);
+        t.setOnClickListener(t1 -> {
+            Intent in = new Intent(view.getContext(), Create_Reminder.class);
+            startActivity(in);
+        });
+        return view;
+        }
+
+}
