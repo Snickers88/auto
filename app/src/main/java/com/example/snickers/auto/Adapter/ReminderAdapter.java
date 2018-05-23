7b81807c74e423a2e6d0c67533e86f47b4e3daf9
Ivannukol <32908822+Ivannukol@users.noreply.github.com>
2018-05-23 15:25:09 +0300

    progress bar and fix

--- app/src/main/java/com/example/snickers/auto/Adapter/ReminderAdapter.java
+++ app/src/main/java/com/example/snickers/auto/Adapter/ReminderAdapter.java
@@ -2,20 +2,28 @@ package com.example.snickers.auto.Adapter;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.snickers.auto.Create.Create_Reminder;
+import com.example.snickers.auto.DB.contacts.ContactDao;
+import com.example.snickers.auto.DB.contacts.ContactModel;
 import com.example.snickers.auto.DB.events.EventDao;
 import com.example.snickers.auto.DB.events.EventModel;
 import com.example.snickers.auto.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.AutoViewHolder> {
     private View v;
@@ -34,22 +42,68 @@ public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.AutoVi
     @Override
     public AutoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.layout_item, parent, false);
+                .inflate(R.layout.item_redminder, parent, false);
         return new ReminderAdapter.AutoViewHolder(v);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onBindViewHolder(@NonNull AutoViewHolder holder, int position) {
         EventModel item = date.get(position);
-        holder.textView.setText(item.getDescription()+"  " + item.getDistance() + "  " +item.getDate() + "");
-        holder.relativelay.setOnLongClickListener(view ->{dialog(v, position); return true;} );
-        holder.relativelay.setOnClickListener(view ->{dialogupdate(v, position); } );
+        int nowDistantion=notification(item),endDistantion=item.getDistance();
+
+        holder.tvDateReminder.setText(item.getDate());
+        holder.tvDistanceReminder.setText(nowDistantion + "/" +endDistantion + "");
+        holder.tvNameReminder.setText(item.getDescription());
+
+        holder.pbReminder.setMax(endDistantion);
+        holder.pbReminder.setProgress(nowDistantion);
+
+        holder.rlReminder.setOnLongClickListener(view -> {
+            dialog(v, position);
+            return true;
+        });
+        holder.rlReminder.setOnClickListener(view -> {
+            dialogupdate(v, position);
+        });
     }
 
+    int notification(EventModel item) {
+        ContactDao contactDao = new ContactDao(v);
+        ArrayList<ContactModel> contactModels = new ArrayList<>();
+
+
+        contactModels.addAll(contactDao.select());
+        SimpleDateFormat format = new SimpleDateFormat("dd:MM:yyyy");
+        int distantion = 0;
+        Date date1 = null;
+        try {
+            date1 = format.parse(item.getDate());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        for (ContactModel itemContact : contactModels) {
+
+            Date date2 = null;
+            try {
+                date2 = format.parse(itemContact.getDate());
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            if (date1 != null && date2 != null && date1.compareTo(date2) <= 0) {
+                distantion += itemContact.getDistance();
+            }
+
+
+        }
+        return distantion;
+    }
 
     private void dialogupdate(View v, int position) {
-        Intent intentupdate = new Intent(v.getContext() , Create_Reminder.class);
-        intentupdate.putExtra("item",date.get(position));
+        Intent intentupdate = new Intent(v.getContext(), Create_Reminder.class);
+        intentupdate.putExtra("item", date.get(position));
         v.getContext().
                 startActivity(intentupdate);
     }
@@ -60,14 +114,18 @@ public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.AutoVi
     }
 
     public class AutoViewHolder extends RecyclerView.ViewHolder {
-        TextView textView;
-        RelativeLayout relativelay;
+        TextView tvDistanceReminder, tvDateReminder, tvNameReminder;
+        RelativeLayout rlReminder;
+        ProgressBar pbReminder;
 
         public AutoViewHolder(View itemView) {
 
             super(itemView);
-            textView = itemView.findViewById(R.id.textView);
-            relativelay = itemView.findViewById(R.id.relativelay);
+            tvDistanceReminder = itemView.findViewById(R.id.tvDistanceReminder);
+            tvDateReminder = itemView.findViewById(R.id.tvDateReminder);
+            tvNameReminder = itemView.findViewById(R.id.tvNameReminder);
+            rlReminder = itemView.findViewById(R.id.rlReminder);
+            pbReminder = itemView.findViewById(R.id.pbReminder);
         }
     }
 
